b9339be0ee37924af1aeb4091ee03ab478a83540
sandi <pal.andrea14@gmail.com>
2020-12-06 17:53:31 +0200

    adding spring boot app

---
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -0,0 +1,15 @@
+package com.pluralsight.conferencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class ConferenceDemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceDemoApplication.class, args);
+	}
+
+}
